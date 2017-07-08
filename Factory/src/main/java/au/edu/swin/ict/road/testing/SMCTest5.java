64efd3d69e1e948db7ee1038fe84692d3cbf9923
indikakumara <indikakumara@users.noreply.github.com>
2017-07-09 00:09:25 +0200

    change scenario 2 - done

---
+++ Factory/src/main/java/au/edu/swin/ict/road/testing/SMCTest5.java
@@ -0,0 +1,47 @@
+package au.edu.swin.ict.road.testing;
+
+import au.edu.swin.ict.road.composite.Composite;
+import au.edu.swin.ict.road.composite.Role;
+import au.edu.swin.ict.road.demarshalling.CompositeDemarshaller;
+import au.swin.ict.research.cs3.road.road4ws.message.MessagePusher;
+import org.apache.axiom.om.OMAbstractFactory;
+import org.apache.axiom.om.OMElement;
+import org.apache.axiom.om.OMFactory;
+import org.apache.axiom.om.OMText;
+import org.apache.axis2.context.ConfigurationContextFactory;
+
+import javax.activation.DataHandler;
+import javax.activation.FileDataSource;
+
+/**
+ * TODO documentation
+ */
+public class SMCTest5 {
+
+    public static void main(String args[]) {
+        try {
+            MessagePusher messagePusher = new MessagePusher(
+                    ConfigurationContextFactory.createConfigurationContextFromFileSystem("sample\\confs\\axis2.xml"),
+                    "AXIS2PushListener", false);
+            CompositeDemarshaller dm = new CompositeDemarshaller();
+            Composite composite = dm.demarshalSMC("src\\main\\resources\\management\\ms2\\smc_source.xml");
+            for (Role role : composite.getRoleMap().values()) {
+                role.registerNewPushListener(messagePusher);
+            }
+            composite.setDefaultPushMessageListener(messagePusher);
+            Thread compo = new Thread(composite);
+            compo.start();
+            Thread.sleep(1000);
+            Thread pe2 = new Thread(new PolicyExecutor(composite, "es2_org", "es2_org_add.drl", true));
+            pe2.start();
+            Thread pe3 = new Thread(new PolicyExecutor(composite, "es2_opr", "es2_opr_add.drl", false));
+            pe3.start();
+            Thread.sleep(5000);
+            System.out.println("Management policy applied nanoseconds");
+            Thread tenant14 = new Thread(new Tenant("HappyTours", composite, 1, "MM"));
+            tenant14.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
